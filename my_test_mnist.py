ae96613a20fa7155974bdea42cbf7d54192a3b29
frhrdr <frederikharder@gmail.com>
2019-10-22 16:13:25 +0200

    MoG training first try

--- my_test_mnist.py
+++ my_test_mnist.py
@@ -11,7 +11,7 @@ from dp_funcs.mog import MoG
 
 
 def main():
-  filename = 'mnist_basic_rmb'
+  filename = 'mnist_mog_rmb'
   act_k = np.power(64.0, 0.125)  # multiplier
   w_nm = 's'  # spectral normalization
   gen = [{'name': 'l1', 'out': 64 * 7 * 7, 'op': 'd', 'act': 'linear', 'act_nm': None, 'out_reshape': [64, 7, 7]},

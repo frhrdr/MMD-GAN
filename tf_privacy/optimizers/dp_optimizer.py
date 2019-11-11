bff20cec6849f5c6529c15317466daf66d864b00
frhrdr <frederikharder@gmail.com>
2019-11-11 16:51:09 +0100

    prepping dp tests

--- tf_privacy/optimizers/dp_optimizer.py
+++ tf_privacy/optimizers/dp_optimizer.py
@@ -77,14 +77,8 @@ def make_optimizer_class(cls):
       # may cause an OOM error.
       self._unroll_microbatches = unroll_microbatches
 
-    def compute_gradients(self,
-                          loss,
-                          var_list,
-                          gate_gradients=GATE_OP,
-                          aggregation_method=None,
-                          colocate_gradients_with_ops=False,
-                          grad_loss=None,
-                          gradient_tape=None):
+    def compute_gradients(self, loss, var_list, gate_gradients=GATE_OP, aggregation_method=None,
+                          colocate_gradients_with_ops=False, grad_loss=None, gradient_tape=None):
       if callable(loss):
         # TF is running in Eager mode, check we received a vanilla tape.
         if not gradient_tape:
@@ -134,22 +128,17 @@ def make_optimizer_class(cls):
         if self._num_microbatches is None:
           self._num_microbatches = tf.shape(loss)[0]
 
-        microbatches_losses = tf.reshape(loss, [self._num_microbatches, -1])
-        sample_params = (
-            self._dp_sum_query.derive_sample_params(self._global_state))
+        microbatches_losses = tf.reshape(loss, shape=[self._num_microbatches, -1])
+        sample_params = (self._dp_sum_query.derive_sample_params(self._global_state))
 
         def process_microbatch(i, sample_state):
           """Process one microbatch (record) with privacy helper."""
-          grads, _ = zip(*super(cls, self).compute_gradients(
-              tf.reduce_mean(tf.gather(microbatches_losses,
-                                       [i])), var_list, gate_gradients,
-              aggregation_method, colocate_gradients_with_ops, grad_loss))
-          grads_list = [
-              g if g is not None else tf.zeros_like(v)
-              for (g, v) in zip(list(grads), var_list)
-          ]
-          sample_state = self._dp_sum_query.accumulate_record(
-              sample_params, sample_state, grads_list)
+          grads, _ = zip(*super(cls, self).compute_gradients(tf.reduce_mean(tf.gather(microbatches_losses, [i])),
+                                                             var_list, gate_gradients, aggregation_method,
+                                                             colocate_gradients_with_ops, grad_loss))
+
+          grads_list = [g if g is not None else tf.zeros_like(v) for (g, v) in zip(list(grads), var_list)]
+          sample_state = self._dp_sum_query.accumulate_record(sample_params, sample_state, grads_list)
           return sample_state
 
         if var_list is None:
@@ -191,34 +180,22 @@ def make_gaussian_optimizer_class(cls):
   class DPGaussianOptimizerClass(make_optimizer_class(cls)):
     """DP subclass of given class cls using Gaussian averaging."""
 
-    def __init__(
-        self,
-        l2_norm_clip,
-        noise_multiplier,
-        num_microbatches=None,
-        ledger=None,
-        unroll_microbatches=False,
-        *args,  # pylint: disable=keyword-arg-before-vararg
-        **kwargs):
-      dp_sum_query = gaussian_query.GaussianSumQuery(
-          l2_norm_clip, l2_norm_clip * noise_multiplier)
+    def __init__(self, l2_norm_clip, noise_multiplier, num_microbatches=None, ledger=None, unroll_microbatches=False,
+                 *args, **kwargs):
+      dp_sum_query = gaussian_query.GaussianSumQuery(l2_norm_clip, l2_norm_clip * noise_multiplier)
 
       if ledger:
-        dp_sum_query = privacy_ledger.QueryWithLedger(dp_sum_query,
-                                                      ledger=ledger)
+        dp_sum_query = privacy_ledger.QueryWithLedger(dp_sum_query, ledger=ledger)
 
-      super(DPGaussianOptimizerClass, self).__init__(
-          dp_sum_query,
-          num_microbatches,
-          unroll_microbatches,
-          *args,
-          **kwargs)
+      super(DPGaussianOptimizerClass, self).__init__(dp_sum_query, num_microbatches, unroll_microbatches,
+                                                     *args, **kwargs)
 
     @property
     def ledger(self):
       return self._dp_sum_query.ledger
 
   return DPGaussianOptimizerClass
+
 
 if LooseVersion(tf.__version__) < LooseVersion('2.0.0'):
   AdagradOptimizer = tf.train.AdagradOptimizer

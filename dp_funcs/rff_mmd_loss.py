d3839f711d6b43ae14ff68853caaad13702be7ea
frhrdr <frederikharder@gmail.com>
2019-11-13 16:54:47 +0100

    testing random fourier feature loss

--- dp_funcs/rff_mmd_loss.py
+++ dp_funcs/rff_mmd_loss.py
@@ -21,14 +21,17 @@ class RFFKMap:
       # with NumpySeedContext(seed=self.seed):
       self.tf_w = tf.constant(np.random.randn(enc_dims, rff_dims // 2) / np.sqrt(rff_sigma * 2.0 ** 0.5))
     else:
-
-      self.tf_w = tf.random_normal(shape=(enc_dims, rff_dims // 2)) / tf.sqrt(tf.cast(rff_sigma * 2.0**0.5, tf.float32))
+      num = tf.random_normal(shape=(enc_dims, rff_dims // 2))
+      den = tf.sqrt(tf.cast(rff_sigma * 2.0**0.5, tf.float32))
+      print('num', num.dtype, 'den', den.dtype)
+      self.tf_w = num / den
+      # self.tf_w = tf.random_normal(shape=(enc_dims, rff_dims // 2)) / tf.sqrt(tf.cast(rff_sigma * 2.0**0.5, tf.float32))
 
   def gen_features(self, encoding):
     # The following block of code is deterministic given seed.
     # Fourier transform formula from http://mathworld.wolfram.com/FourierTransformGaussian.html
 
-
+    print(encoding.dtype, self.tf_w.dtype)
     enc_w = tf.matmul(encoding, self.tf_w)  # (bs, d_enc) (d_enc, rff) -> (bs, rff)
     enc_z1 = tf.math.cos(enc_w)  # (bs, rff)
     enc_z2 = tf.math.sin(enc_w)  # (bs, rff)

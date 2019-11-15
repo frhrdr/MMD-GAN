abe6c61b78d16fd13244cc18ee783c5e62cdd81f
frhrdr <frederikharder@gmail.com>
2019-11-15 20:47:26 +0100

    prepping DP random fourier feature loss (now tested independently)

--- dp_funcs/dp_grads.py
+++ dp_funcs/dp_grads.py
@@ -88,9 +88,11 @@ def single_grad(loss, optimizer, var_list):
 def sample_grads(sample_loss, optimizer, var_list):
   # all gradiend beloning to one sample (i.e. #RFF many)
   print('---------------------------calling sample grads')
+  time.sleep(1)
   n_rff = sample_loss.get_shape()[0]
   grads = [single_grad(sample_loss[i], optimizer, var_list) for i in range(n_rff)]
   print('---------------------------stacking sample grads')
+  time.sleep(1)
   grad_stack = [tf.stack(k) for k in zip(*grads)]
   print('---------------------------returning sample grads')
   time.sleep(1)

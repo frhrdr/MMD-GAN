dp-grad was off by factor 2, fixed that

DeepLearning/my_sngan.py
@@ -209,7 +209,7 @@ class SNGan(object):
             vars_gen = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, "gen")
 
             if dp_spec is not None:
-                grads_list, loss_list = dp_compute_grads(loss_ops, opt_ops, dp_spec, vars_dis, vars_gen)
+                grads_list, loss_list = dp_compute_grads(loss_ops, dp_spec, vars_dis, vars_gen)
                 loss_ops = self.loss_tuple(loss_list[0], loss_list[1])
             else:
                 # compute gradient: grads is a list of (gradient, variable) tuples

dp_funcs/dp_grads.py
@@ -105,7 +105,7 @@ def compose_full_grads(fx_dp, fy, dfx_dp, dfy, batch_size):
   :param batch_size: for normalization
   :return:
   """
-  loss_diff_normed = (fx_dp - fy) / batch_size**2  # (rff_dims)
+  loss_diff_normed = 2 * (fx_dp - fy) / batch_size**2  # (rff_dims)
 
   def full_grad(x, y):
     # print('grad_x_shape', x.get_shape())  # (rff, w_dims)
@@ -147,7 +147,7 @@ def loss_dis_from_rff(rff_dis_loss, rff_gen_loss, batch_size):
   return tf.reduce_sum((tf.reduce_sum(rff_dis_loss, axis=0) - rff_gen_loss)**2) / batch_size**2
 
 
-def dp_compute_grads(loss_ops, opt_ops, dp_spec, vars_dis, vars_gen):
+def dp_compute_grads(loss_ops, dp_spec, vars_dis, vars_gen):
   """
   computes dp gradients for discriminator update with rff mmd estimator
 
@@ -175,7 +175,8 @@ def dp_compute_grads(loss_ops, opt_ops, dp_spec, vars_dis, vars_gen):
   loss_dis = loss_dis_from_rff(loss_ops.dis.fdat, loss_ops.dis.fgen, batch_size)
 
   # generator op:
-  grads_n_vars_gen = opt_ops.gen.compute_gradients(loss_ops.gen, var_list=vars_gen)
+  # grads_n_vars_gen = opt_ops.gen.compute_gradients(loss_ops.gen, var_list=vars_gen)
+  grads_n_vars_gen = list(zip(tf.gradients(loss_ops.gen, vars_gen), vars_dis))
 
   grads_list = [grads_n_vars_dis, grads_n_vars_gen]
   loss_list = [loss_dis, loss_ops.gen]

dp_grads_test_copy.py
@@ -6,31 +6,31 @@ from collections import namedtuple
 
 
 def test_dp_rff_grads():
-  optimizer = tf.train.AdamOptimizer()
   x = tf.constant(np.random.randn(3, 4).astype(np.float32))  # 3 samples of dim 4
   w = tf.Variable(np.ones((4, 2)).astype(np.float32))  # a mapping from d=4 to d=2
   b = tf.Variable(np.ones(2).astype(np.float32))
   loss = x @ w + b  # 2d loss for 3 samples
 
-  grads = dp_rff_gradients(optimizer, loss, [w, b], l2_norm_clip=tf.constant(10.), noise_factor=tf.constant(0.01))
+  dp_grads = dp_rff_gradients(loss, [w, b], l2_norm_clip=tf.constant(10.), noise_factor=tf.constant(0.0))
+  normal_grads = tf.gradients(loss, [w, b])
   sess = tf.Session()
-  x_mat, g_mat = sess.run([x, grads])
+  x_mat, dp_g_mat, nrm_g_mat = sess.run([x, dp_grads, normal_grads])
   print(x_mat)
-  print(g_mat)
+  print('DP-grads')
+  print(dp_g_mat)
+  print('Normal-grads')
+  print(nrm_g_mat)
 
 
 def test_dp_compute_grads():
-  opt_gen = tf.train.AdamOptimizer()
-  opt_dis = tf.train.AdamOptimizer()
-  opt_ops = namedtuple('opt', ['gen', 'dis'])(opt_gen, opt_dis)
 
   dp_spec = {'loss_clip': 10.,
              'grad_clip': 10.,
-             'loss_noise': 0.1,
-             'grad_noise': 0.1}
-  n_samples = 53
-  d_in = 10
-  d_out = 5
+             'loss_noise': 0.,
+             'grad_noise': 0.}
+  n_samples = 3
+  d_in = 4
+  d_out = 2
 
   x = tf.constant(np.random.randn(n_samples, d_in).astype(np.float32))  # 3 samples of dim 4
   w = tf.Variable(np.ones((d_in, d_out)).astype(np.float32))  # a mapping from din to dout
@@ -42,49 +42,27 @@ def test_dp_compute_grads():
   d = tf.Variable(np.ones(d_out).astype(np.float32))
   loss2 = tf.reduce_sum(y @ v + d, axis=0)  # 2d loss sum over 3 samples
 
-  rff_loss = namedtuple('opt', ['fdat', 'fgen'])(loss1, loss2)
-  loss_ops = namedtuple('loss', ['gen', 'dis'])(tf.reduce_sum(loss2), rff_loss)
-  grads_list, loss_list = dp_compute_grads(loss_ops, opt_ops, dp_spec, vars_dis=[w, b, d], vars_gen=[y, v])
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
-  l_dis, l_gen = sess.run(loss_list)
-  print(l_dis, l_gen)
-  g_dis, g_gen = sess.run(grads_list)
-  print(g_dis)
-  print(g_gen)
-
-
-def test_dp_compute_grads_v2():
-  opt_gen = tf.train.AdamOptimizer()
-  opt_dis = tf.train.AdamOptimizer()
-  opt_ops = namedtuple('opt', ['gen', 'dis'])(opt_gen, opt_dis)
-
-  dp_spec = {'loss_clip': 10.,
-             'grad_clip': 10.,
-             'loss_noise': 0.1,
-             'grad_noise': 0.1}
-
-  x = tf.constant(np.random.randn(3, 4).astype(np.float32))  # 3 samples of dim 4
-  w = tf.Variable(np.ones((4, 2)).astype(np.float32))  # a mapping from d=4 to d=2
-  b = tf.Variable(np.ones(2).astype(np.float32))
-  loss1 = x @ w + b  # 2d loss for 3 samples
-
-  y = tf.constant(np.random.randn(3, 4).astype(np.float32))  # 3 samples of dim 4
-  v = tf.Variable(np.ones((4, 2)).astype(np.float32))  # a mapping from d=4 to d=2
-  d = tf.Variable(np.ones(2).astype(np.float32))
-  loss2 = tf.reduce_sum(y @ v + d, axis=0)  # 2d loss sum over 3 samples
+  full_loss = tf.norm(tf.reduce_sum(loss1, axis=0) - loss2)**2 / n_samples**2
+  full_grad = tf.gradients(full_loss, [w, b, d])
 
   rff_loss = namedtuple('opt', ['fdat', 'fgen'])(loss1, loss2)
   loss_ops = namedtuple('loss', ['gen', 'dis'])(tf.reduce_sum(loss2), rff_loss)
-  grads_list, loss_list = dp_compute_grads(loss_ops, opt_ops, dp_spec, vars_dis=[w, b, d], vars_gen=[y, v])
+  grads_list, loss_list = dp_compute_grads(loss_ops, dp_spec, vars_dis=[w, b, d], vars_gen=[y, v])
   sess = tf.Session()
   sess.run(tf.global_variables_initializer())
-  l_dis, l_gen = sess.run(loss_list)
-  print(l_dis, l_gen)
-  g_dis, g_gen = sess.run(grads_list)
-  print(g_dis)
-  print(g_gen)
-
+  l_dis, l_gen, l_full, l_1, l_2 = sess.run(loss_list + [full_loss, loss1, loss2])
+  # print('loss components')
+  # print(l_1, l_2)
+  print('losses')
+  print(l_dis, l_full)
+  g_dis, g_gen, g_full = sess.run(grads_list + [full_grad])
+  print('grads_compound')
+  for g in g_dis:
+    print(g[0])
+  print('grads_direct')
+  for g in g_full:
+    print(g)
 
 if __name__ == '__main__':
   test_dp_compute_grads()
+  # test_dp_rff_grads()
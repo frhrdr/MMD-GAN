a6ec8870f4ae559b532bd588561c34e1fa261033
frhrdr <frederikharder@gmail.com>
2019-12-18 12:43:38 +0100

    dp em for MoG ready for testing - added run args

--- test_main.py
+++ test_main.py
@@ -57,7 +57,7 @@ def main(ar):
   else:
     np_mog = default_mogs(ar.mog_type, ar.n_comp, d_enc, ar.cov_type, ar.decay_gamma, ar.em_steps, ar.map_em,
                           ar.reg_covar,
-                          ar.l2_norm_clip_mog, ar.pi_nf, ar.mu_nf, ar.sig_nf)
+                          ar.l2_norm_clip_mog, ar.noise_factor_mog_pi, ar.noise_factor_mog_mu, ar.noise_factor_mog_sig)
     mog_model = EncodingMoG(d_enc, ar.n_comp, linked_gan=mdl, np_mog=np_mog, n_data_samples=n_data_samples,
                             enc_batch_size=200, filename=ar.filename, cov_type=ar.cov_type,
                             fix_cov=ar.fix_cov, fix_pi=ar.fix_pi, re_init_at_step=ar.re_init_step)
